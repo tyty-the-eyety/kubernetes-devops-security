32773287a03978f525dfd25599c0eb0c518b89d1
tyty-the-eyety <tyronesch@gmail.com>
2023-07-15 12:32:55 +0100

    reverting java

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -14,7 +14,7 @@ public class NumericController {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 	//private static final String baseURL = "http://localhost:5000/plusone";
-	private static final String baseURL = "http://localhost:5000/plusone";
+	private static final String baseURL = "http://localhost:5000/increment";
 	
 	RestTemplate restTemplate = new RestTemplate();
 	
