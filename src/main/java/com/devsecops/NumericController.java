bb6bb1df776b44ad8d925d4f605d32f392f8cd52
tyty-the-eyety <tyronesch@gmail.com>
2023-07-15 12:24:19 +0100

    reverting java

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -13,7 +13,8 @@ import org.springframework.web.client.RestTemplate;
 public class NumericController {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
-	private static final String baseURL = "http://localhost:5000/plusone";
+	//private static final String baseURL = "http://localhost:5000/plusone";
+	private static final String baseURL = "http://localhost:5000/";
 	
 	RestTemplate restTemplate = new RestTemplate();
 	
