35bae26303324dd3055251571e053e14ed67ce2a
tyty-the-eyety <tyronesch@gmail.com>
2023-07-15 19:53:55 +0100

    zap ignores

--- zap.sh
+++ zap.sh
@@ -5,13 +5,13 @@ PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].node
 # first run this
 chmod 777 $(pwd)
 echo $(id -u):$(id -g)
-docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
+docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap_rules -r zap_report.html 
 
 exit_code=$?
 
 # HTML Report
  mkdir -p owasp-zap-report
- mv zap_report.html owasp-zap-report
+ cp zap_report.html owasp-zap-report
 
 
 echo "Exit Code : $exit_code"
